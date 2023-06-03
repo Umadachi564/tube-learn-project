6f73ee0d255adf67bed85f6059b81402bf182c34
Umadachi564 <takuya.19980713@gmail.com>
2023-06-03 11:13:00 +0900

    add download link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import '@aws-amplify/ui-react/styles.css'; 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Auth, Storage} from 'aws-amplify';
+
 
 const api_url = "https://2e65g038x5.execute-api.ap-northeast-1.amazonaws.com/staging/tubescript-staging"
-function CreateScript(SetToppage,url_data,SetUrl){
+
+function CreateScript(SetToppage,url_data,SetUrl,SetDownlink){
   const onUrlChange = (event)=>{
     const v =event.target.value;
       SetUrl(v);
@@ -26,6 +28,25 @@ function CreateScript(SetToppage,url_data,SetUrl){
       });
   }
 
+  const OnDclick = () => {
+    try{
+      const url_split = url_data.split('/');
+      const video_id = url_split[url_split.length-1];
+      const filename = video_id + '.txt'
+      Storage.get(filename,{
+        level: 'public',
+      }).then(data =>{
+          console.log(data)
+          SetDownlink(
+            <a href={data} target="_blank" rel="noreferrer">
+              テキストファイル
+            </a>
+          )
+      })
+    }catch(error){
+      alert('ファイルがダウンロードできません. URLを確認してください');
+    }
+  }
   SetToppage(
     <div>
       <h2>サンプル</h2>
@@ -36,20 +57,28 @@ function CreateScript(SetToppage,url_data,SetUrl){
           <input type="text" name="url" onChange={onUrlChange} />
         </label>
         <button type="submit" onClick={OnClick}>Add</button>
+        <button onClick={OnDclick}>Download</button>
+        
       </div>
     </div>
   )
 }
+
 function App() {
   const [toppage, SetToppage] = useState("");
+  const [downloadlink, SetDownlink] = useState("");
   const [url_data, SetUrl] = useState("");
+  const [progress, SetProgress] = useState(0);
 
+  
   useEffect(()=>{
-    CreateScript(SetToppage,url_data,SetUrl);
-  },[url_data]);
+    CreateScript(SetToppage,url_data,SetUrl,SetDownlink);
+  },[url_data,progress,downloadlink]);
+
   return (
     <div className="App">
       {toppage}
+      {downloadlink}
     </div>
   );
 }
